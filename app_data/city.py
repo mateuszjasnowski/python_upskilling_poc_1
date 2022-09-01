2cb9a4444062d930f64c252ef92f1a9e970ee924
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-09-01 16:51:47 +0200

    black and pylint correcitons

--- app_data/city.py
+++ app_data/city.py
@@ -1,15 +1,19 @@
-'''
+"""
 Module contains City class and all needed methods to operate with them
-'''
+"""
 import os
 import shutil
 import zipfile
 import urllib.request
 
-class CityData():
+
+class CityData:
     """CityData downloaded from web"""
-    def __init__(self, city_name: str, city_url: str, download_mode: bool = True) -> None:
-        '''
+
+    def __init__(
+        self, city_name: str, city_url: str, download_mode: bool = True
+    ) -> None:
+        """
         Download city's data from web and upload to DB
         For city's data representation, use City class
         Actions in __init__:
@@ -17,21 +21,21 @@ class CityData():
         - unpack zip archive
         - get needed data from files inside
         - upload data to db
-        '''
+        """
         self.errors = []
         self.return_code = 201
 
-        temp_city_dict = '.temp_city/'
+        temp_city_dict = ".temp_city/"
         try:
-            if download_mode == True:
+            if download_mode is True:
                 print(f"ACTION: Downloading files for {city_name} from {city_url} ...")
                 zip_file_name = ".temp_city.zip"
-                temp_city_dict = '.temp_city/'
+                temp_city_dict = ".temp_city/"
 
-                #download the file
+                # download the file
                 urllib.request.urlretrieve(city_url, zip_file_name)
 
-                #unzip the file
+                # unzip the file
                 with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
                     zip_ref.extractall(temp_city_dict)
 
@@ -41,21 +45,26 @@ class CityData():
             self.errors.append("Cannot download feed")
             self.return_code = 500
         else:
-            #opening files
+            # opening files
             try:
-                with open(temp_city_dict+'routes.txt', 'r', encoding="UTF-8") as routes_file:
+                with open(
+                    temp_city_dict + "routes.txt", "r", encoding="UTF-8"
+                ) as routes_file:
                     file_lines = routes_file.read().replace('"', "").split("\n")
                     file_columns = file_lines[0].split(",")
-                self.routes = [dict(zip(file_columns, line.split(','))) for line in file_lines[1:]]
+                self.routes = [
+                    dict(zip(file_columns, line.split(","))) for line in file_lines[1:]
+                ]
 
             except FileNotFoundError as not_found:
-                self.errors.append(f'Cannot open file {not_found.filename}')
+                self.errors.append(f"Cannot open file {not_found.filename}")
                 self.return_code = 500
             finally:
                 self.name = city_name
-                if download_mode == True:
+                if download_mode is True:
                     os.remove(zip_file_name)
                     shutil.rmtree(temp_city_dict)
 
-    def items(self) -> str:
-        return {'name': self.name, 'num or routes': len(self.routes)}
+    def items(self) -> dict:
+        """Returing dict with visible items"""
+        return {"name": self.name, "num or routes": len(self.routes)}
--- app_data/endpoints.py
+++ app_data/endpoints.py
@@ -1,31 +1,37 @@
-import urllib.request
-import zipfile
-import os
-
+"""Endpoint for api"""
 from flask import request
 
 from app_data import app
 from app_data.city import CityData
 
 
-@app.route('/', methods=["GET"])
-@app.route('/version', methods=["GET"])
+@app.route("/", methods=["GET"])
+@app.route("/version", methods=["GET"])
 def get_version():
-    return {'version': "0.0.1"}
+    """Returns api and feeds version"""
+    return {"version": "0.0.1"}
+
 
 @app.route("/cities/create_city", methods=["POST"])
 def cities_create_city():
+    """
+    Checking if city exist TODO
+    Downloading data
+    Uploading data to DB
+    """
     request_data = request.headers
-    city_name = request_data.get('city_name')
-    city_url = request_data.get('city_url')
+    city_name = request_data.get("city_name")
+    city_url = request_data.get("city_url")
 
-    if request_data.get('download_mode'):
-        download_mode = request_data.get('download_mode')
+    if request_data.get("download_mode"):
+        download_mode = request_data.get("download_mode")
     else:
         download_mode = True
 
-    city_data = CityData(city_name=city_name, city_url=city_url, download_mode=download_mode)
+    city_data = CityData(
+        city_name=city_name, city_url=city_url, download_mode=download_mode
+    )
     if city_data.return_code != 201:
-        return {'Status': "Failed", 'Errors': city_data.errors}, city_data.return_code
+        return {"Status": "Failed", "Errors": city_data.errors}, city_data.return_code
 
-    return {'Status': "Success", 'content': city_data.items()}, 201
+    return {"Status": "Success", "content": city_data.items()}, 201

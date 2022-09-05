e2bc45ce9f62499f0f897bc1885e9453cbd67464
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-09-05 15:17:59 +0200

    created db models

--- app_data/city.py
+++ app_data/city.py
@@ -1,4 +1,167 @@
 """
-Module contains City class and all needed methods to operate with them
+Module contains db classes for tables:
+City - indicating city
+Agency
+Calendar
+Calendar_dates #MISS THAT
+Control_stops
+Route_types
+Routes
+Shapes #MISS
+Stop_times
+Stops
+Trips
+Variants
+Vechicle_types
 """
+from app_data import db
 
+
+class City(db.Model):
+    """city table"""
+
+    city_id = db.Column(db.Integer, primary_key=True)
+    city_name = db.Column(db.String(), unique=True, nullable=False)
+    feed_publisher_name = db.Column(db.String())
+    feed_publisher_url = db.Column(db.String())
+    feed_lang = db.Column(db.String())
+    feed_start_date = db.Column(db.DateTime)
+    feed_end_date = db.Column(db.DateTime)
+
+    agencies = db.relationship("Agency", backref="agencies", lazy=True)
+
+    def __repr__(self):
+        return f"City('{self.id}', '{self.city_name}', '{self.feed_publisher_name}', '{self.feed_publisher_url}', '{self.feed_lang}', '{self.feed_start_date}', '{self.feed_end_date}')"
+
+
+class Agency(db.Model):
+    """agency table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    agency_id = db.Column(db.Integer, primary_key=True)
+    agency_name = db.Column(db.String())
+    agency_url = db.Column(db.String())
+    agency_timezone = db.Column(db.String())
+    agency_phone = db.Column(db.String())
+    agency_lang = db.Column(db.String())
+
+    # TODO __repr__
+
+
+class Calendar(db.Model):
+    """calendar table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    service_id = db.Column(db.Integer, primary_key=True)
+    monday = db.Column(db.Integer)
+    tuesday = db.Column(db.Integer)
+    wednesday = db.Column(db.Integer)
+    thursday = db.Column(db.Integer)
+    friday = db.Column(db.Integer)
+    saturday = db.Column(db.Integer)
+    sunday = db.Column(db.Integer)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+
+
+class ControlStop(db.Model):
+    """control_stop table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    variant_id = db.Column(db.Integer, primary_key=True)
+    stop_id = db.Column(db.Integer, db.ForeignKey("stop.stop_id"), nullable=False)
+
+
+class RouteType2(db.Model):
+    """route_type2 table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    route_type2_id = db.Column(db.Integer, primary_key=True)
+    route_type2_name = db.Column(db.String())
+
+    def __repr__(self) -> str:
+        return f"RouteType2('{self.city_id.city_name}', '{self.route_type2_id}', '{self.route_type2_name}'"
+
+
+class Route(db.Model):
+    """route table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    route_id = db.Column(db.String(), primary_key=True)
+    agency_id = db.Column(db.Integer, db.ForeignKey("agency.agency_id"), nullable=False)
+    route_short_name = db.Column(db.String())
+    route_long_name = db.Column(db.String())
+    route_desc = db.Column(db.Text)
+    route_type = db.Column(db.Integer)
+    route_type2_id = db.Column(
+        db.Integer, db.ForeignKey("route_type2.route_type2_id"), nullable=False
+    )
+    valid_from = db.Column(db.DateTime)
+    valid_until = db.Column(db.DateTime)
+
+
+class StopTime(db.Model):
+    """stop_time table"""
+
+    stop_time_id = db.Column(db.Integer, primary_key=True)
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    trip_id = db.Column(db.String(), db.ForeignKey("trip.trip_id"), nullable=False)
+    arrival_time = db.Column(db.Time)
+    departure_time = db.Column(db.Time)
+    stop_id = db.Column(db.Integer, db.ForeignKey("stop.stop_id"), nullable=False)
+    stop_sequence = db.Column(db.Integer)
+    pickup_type = db.Column(db.Integer)
+    drop_off_type = db.Column(db.Integer)
+
+
+class Stop(db.Model):
+    """stop table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    stop_id = db.Column(db.Integer, primary_key=True)
+    stop_code = db.Column(db.String())
+    stop_name = db.Column(db.String())
+    stop_lat = db.Column(db.String())
+    stop_lon = db.Column(db.String())
+
+
+class Trip(db.Model):
+    """trip table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    route_id = db.Column(db.String(), db.ForeignKey("route.route_id"), nullable=False)
+    service_id = db.Column(
+        db.Integer, db.ForeignKey("calendar.service_id"), nullable=False
+    )
+    trip_id = db.Column(db.String(), primary_key=True)
+    trip_headsign = db.Column(db.String())
+    direction_id = db.Column(db.Integer)
+    shape_id = db.Column(db.Integer)  # not integrarting shapes
+    brigade_id = db.Column(db.Integer)
+    vechicle = db.Column(
+        db.Integer, db.ForeignKey("vechicle_type.vehicle_type_id"), nullable=False
+    )
+    variant_id = db.Column(
+        db.Integer, db.ForeignKey("variant.variant_id"), nullable=False
+    )
+
+
+class Variant(db.Model):
+    """variant table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    variant_id = db.Column(db.Integer, primary_key=True)
+    is_main = db.Column(db.Integer)
+    equiv_main_variant_id = db.Column(db.Integer)
+    join_stop_id = db.Column(db.Integer)
+    disjoin_stop_id = db.Column(db.Integer)
+
+
+class VechicleType(db.Model):
+    """vechicle_type table"""
+
+    city_id = db.Column(db.Integer, db.ForeignKey("city.city_id"), nullable=False)
+    vehicle_type_id = db.Column(db.Integer, primary_key=True)
+    vehicle_type_name = db.Column(db.String())
+    vehicle_type_description = db.Column(db.String())
+    vehicle_type_symbol = db.Column(db.String())

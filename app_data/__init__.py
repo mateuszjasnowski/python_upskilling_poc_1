09a3ec8aa1c5a6037e79ba7cda10ba3550e99682
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-09-05 10:28:33 +0200

    Create db connection

--- app_data/__init__.py
+++ app_data/__init__.py
@@ -1,7 +1,11 @@
 """init Flask app & import endpoints"""
-
 from flask import Flask  # pylint: disable=import-error
+from flask_sqlalchemy import SQLAlchemy
+
+from app_data.secrets import DB_URL
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+db = SQLAlchemy(app)
 
 from app_data import endpoints  # pylint: disable=wrong-import-position
---
+++ app_data/secrets.template.py
@@ -0,0 +1,10 @@
+"""
+Secrets and constants for app
+TEMPLATE FILE
+"""
+#DATABASE
+DB_USER = ''
+DB_USER_PASS = ''
+DB_ADDR = ''
+
+DB_URL = f'postgres://{DB_USER}:{DB_USER_PASS}@{DB_ADDR}'

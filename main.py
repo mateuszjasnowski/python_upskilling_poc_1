e6bdbf1564a590a987e10337e6d5247b5ca4ac57
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-09-07 15:17:31 +0200

    Correcting endpoints

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """PoC API App for MPK data"""
 from app_data import app
-from app_data.secrets import API_URL
+from app_data.secrets import API_URL, API_PORT
 
 
 def main():
     """
     TODO
     """
-    app.run(host=API_URL, debug=True)
+    app.run(host=API_URL, port=API_PORT, ssl_context='adhoc', debug=True)
 
 
 if __name__ == "__main__":

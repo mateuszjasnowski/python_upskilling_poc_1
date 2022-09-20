13364c1cfee529cd79e43f4ca2dad084eb828ceb
Mateusz Jasnowski <mateuszjasnowski99@gmail.com>
2022-09-20 16:33:06 +0200

    First unittests for 0.2.0 api

---
+++ unittests/test_find_route.py
@@ -0,0 +1,12 @@
+"""
+Unittests for find_route module
+Testing FindRoute class with all methods
+"""
+import unittest
+
+from app_data.find_route import FindRoute
+
+class TestFindRoute(unittest.TestCase):
+    """Testing FindRoute class"""
+
+    pass #TODO
